7e123a04117e6df2beeb88374fdf4100dee42d84
KLUTESSTREAMPC\KlutesStreamRig <ktulue.bot@gmail.com>
2026-03-14 13:34:11 -0400

    feat: set onboarding pending flags on first install

--- src/background/serviceWorker.ts
+++ src/background/serviceWorker.ts
@@ -1,1 +1,11 @@
-// No handlers needed; popup.html is declared as default_popup in manifest.json
+import { ONBOARDING_KEYS } from '../shared/types';
+
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install') {
+    chrome.storage.local.set({
+      [ONBOARDING_KEYS.wizardPending]: true,
+      [ONBOARDING_KEYS.phase2Pending]: true,
+      [ONBOARDING_KEYS.complete]: false,
+    });
+  }
+});
